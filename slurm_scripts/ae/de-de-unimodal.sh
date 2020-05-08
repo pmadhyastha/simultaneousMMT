cb50c6822bfe3259fc074f5a741f9f8393d858bc
al4616 <al4616@ic.ac.uk>
2020-05-08 18:25:30 +0100

    de-de bpe unimodal experiment script added

--- slurm_scripts/ae/de-de-unimodal.sh
+++ slurm_scripts/ae/de-de-unimodal.sh
@@ -3,13 +3,13 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=al4616
-#SBATCH -o ./simultaneousMMT/experiments/custom/de-de-slurm-%j.out
+#SBATCH -o ./simultaneousMMT/experiments/custom/de-de-unimodal-slurm-%j.out
 
 export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
 source activate
 conda activate simnmt
 cd simmt
 TERM=xterm
-CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/de-de.conf train.seed:0
+CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/de-de-unimodal.conf train.seed:0
 /usr/bin/nvidia-smi
 uptime
