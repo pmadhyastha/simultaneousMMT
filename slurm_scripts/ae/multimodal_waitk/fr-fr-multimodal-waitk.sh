2f561bf3688d893df3ec1946819d9b216815d025
Andy Li <andy.li@artificial.io>
2020-05-19 18:59:41 +0100

    waitk confs and scripts set up, restructured ae script directory

---
+++ slurm_scripts/ae/multimodal_waitk/fr-fr-multimodal-waitk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=al4616
+#SBATCH -o ./simultaneousMMT/experiments/custom/fr-fr-multimodal-waitk-slurm-%j.out
+
+export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
+source activate
+conda activate simnmt
+cd simmt
+TERM=xterm
+
+for k in 1 2 3 5 7
+do
+    CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/fr-fr-multimodal-w2bpe-wait{$k}.conf train.seed:0
+    /usr/bin/nvidia-smi
+done
+uptime
