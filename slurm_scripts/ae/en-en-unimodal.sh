8ef6e8ebf08b5386fa2deb58ef04cacda15ec8df
al4616 <al4616@ic.ac.uk>
2020-05-19 02:12:44 +0100

    en-en experiments finished with w2bpe

--- slurm_scripts/ae/en-en-unimodal.sh
+++ slurm_scripts/ae/en-en-unimodal.sh
@@ -3,13 +3,13 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=al4616
-#SBATCH -o ./simultaneousMMT/experiments/custom/en-en-multimodal-slurm-%j.out
+#SBATCH -o ./simultaneousMMT/experiments/custom/en-en-unimodal-slurm-%j.out
 
 export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
 source activate
 conda activate simnmt
 cd simmt
 TERM=xterm
-CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/en-en-multimodal-w2bpe.conf train.seed:0
+CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/en-en-unimodal-w2bpe.conf train.seed:0
 /usr/bin/nvidia-smi
 uptime
