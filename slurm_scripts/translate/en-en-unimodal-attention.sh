df73a00bf736889380efe3abbb1e0cf3766bf6d3
al4616 <al4616@ic.ac.uk>
2020-05-19 04:47:15 +0100

    dumped attention of en-en unimodal

--- slurm_scripts/translate/en-en-unimodal-attention.sh
+++ slurm_scripts/translate/en-en-unimodal-attention.sh
@@ -3,12 +3,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=al4616
-#SBATCH -o ./simultaneousNMT/experiments/custom/en-en-unimodal-dump-attention-slurm-%j.out
+#SBATCH -o ./simultaneousMMT/experiments/custom/en-en-unimodal-dump-attention-slurm-%j.out
 
 export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
 source activate
 conda activate simnmt
 cd simmt
 TERM=xterm
-ls ~
-CUDA_VISIBLE_DEVICES=0 nmtpy stranslate -m 100 -s test_2016_flickr -f vwk --n-init-tokens "1,2,3,5,7" -o ../simultaneousNMT/en_en_unimodal_decoding_results ~/experiments/experiments/simnmt/nmtpy/bpe_models/en-en/en-en-unimodal-w2bpe/simultaneousnmt-r65ec1-val026.best.bleu_93.400.ckpt
+CUDA_VISIBLE_DEVICES=0 nmtpy stranslate -m 100 -s test_2016_flickr -f wk --n-init-tokens "1,2,3,5,7" -o ../simultaneousMMT/decoding_results/en_en_unimodal ../simultaneousMMT/experiments/simultaneousnmt-r65ec1.best.bleu.ckpt
