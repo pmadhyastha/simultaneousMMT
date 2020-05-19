839d2dd258390431bebcd78937194f677a696b0d
Andy Li <andy.li@artificial.io>
2020-05-19 02:29:32 +0100

    amended filepath of ckpt

--- slurm_scripts/translate/en-en-unimodal-attention.sh
+++ slurm_scripts/translate/en-en-unimodal-attention.sh
@@ -11,4 +11,4 @@ conda activate simnmt
 cd simmt
 TERM=xterm
 ls ~
-CUDA_VISIBLE_DEVICES=0 nmtpy stranslate -m 100 -s test_2016_flickr -f vwk --n-init-tokens "1,2,3,5,7" -o ../simultaneousNMT/en_en_unimodal_decoding_results ~/experiments/en-en/en-en-unimodal-w2bpe/simultaneousnmt-r65ec1-val026.best.bleu_93.400.ckpt
+CUDA_VISIBLE_DEVICES=0 nmtpy stranslate -m 100 -s test_2016_flickr -f vwk --n-init-tokens "1,2,3,5,7" -o ../simultaneousNMT/en_en_unimodal_decoding_results ~/experiments/experiments/simnmt/nmtpy/bpe_models/en-en/en-en-unimodal-w2bpe/simultaneousnmt-r65ec1-val026.best.bleu_93.400.ckpt
