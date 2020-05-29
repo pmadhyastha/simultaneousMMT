ce4e310b83edb11267ebdea27f010f06938c3482
Andy Li <andy.li@artificial.io>
2020-05-29 21:46:43 +0100

    added multimodal incongruent experiment configs

--- slurm_scripts/ae/multimodal_shuffled_waitk/de-de-multimodal-shuffled-waitk.sh
+++ slurm_scripts/ae/multimodal_shuffled_waitk/de-de-multimodal-shuffled-waitk.sh
@@ -3,16 +3,16 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=al4616
-#SBATCH -o ./simultaneousMMT/experiments/custom/de-de-multimodal-waitk-slurm-%j.out
+#SBATCH -o ./simultaneousMMT/experiments/custom/de-de-multimodal-shuffled-waitk-slurm-%j.out
 
 export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
 source activate
 conda activate simnmt
 cd simmt
 TERM=xterm
-for k in 5 7
+for k in 1 2 3 5 7
 do
-    CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/de-de-multimodal-w2bpe-wait$k.conf train.seed:0
+    CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/de-de-multimodal-shuffled-w2bpe-wait$k.conf train.seed:0
     /usr/bin/nvidia-smi
 done
 uptime
