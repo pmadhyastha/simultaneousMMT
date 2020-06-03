32c8427e5c16249273f0e8a336e6cfa03b4b5c7c
al4616 <al4616@ic.ac.uk>
2020-06-03 19:56:52 +0100

    run benchmark and delay analysis on en-fr waitk

--- slurm_scripts/multimodal/en-fr-multimodal-waitk.sh
+++ slurm_scripts/multimodal/en-fr-multimodal-waitk.sh
@@ -3,16 +3,16 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=al4616
-#SBATCH -o ./simultaneousMMT/experiments/custom/en-fr-unimodal-waitk-slurm-%j.out
+#SBATCH -o ./simultaneousMMT/experiments/custom/en-fr-multimodal-waitk-slurm-%j.out
 
 export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
 source activate
-conda activate simnmt
+conda activate simnmt-ozan
 cd simmt
 TERM=xterm
 for k in 1 2 3 5 7
 do
-    CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/en-fr/wait$k-rnn-unimodal.conf
+    CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/en-fr/wait$k-rnn-multimodal-decatt.conf
     /usr/bin/nvidia-smi
 done
 uptime
---
+++ slurm_scripts/translate/decode_en_fr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=al4616
+#SBATCH -o ./simultaneousMMT/experiments/custom/en-fr-decode-all-waitk-slurm-%j.out
+
+export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
+source activate
+conda activate simnmt-ozan
+cd ~/experiments/simnmt/nmtpy/w2w_models/en-de
+ls
+/vol/bitbucket/al4616/simmt/scripts/decode_nmt_test_waitk.sh
+/vol/bitbucket/al4616/simmt/scripts/delay_analysis.py -r /vol/bitbucket/al4616/simmt/data/multi30k/en-fr/test_2016_flickr.lc.norm.tok.fr
