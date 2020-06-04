bbc6610980b2fb6e3c53fad7800c7969a7bae94f
al4616 <al4616@ic.ac.uk>
2020-06-04 15:32:51 +0100

    redo delay analysis, starting multimodal RL

---
+++ slurm_scripts/rl/en-fr-multimodal-env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=al4616
+#SBATCH -o ./simultaneousMMT/experiments/rl/en-fr-multimodal-rlenv-slurm-%j.out
+
+export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
+source activate
+conda activate simnmt-ozan
+cd simmt
+TERM=xterm
+CUDA_VISIBLE_DEVICES=0 nmtpy train -C configs/en-fr/word2word/multimodal-decatt/snmt-rnn-multimodal-decatt.conf train.seed:0
+/usr/bin/nvidia-smi
+uptime
--- slurm_scripts/translate/decode_en_fr.sh
+++ slurm_scripts/translate/decode_en_fr.sh
@@ -8,7 +8,7 @@
 export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
 source activate
 conda activate simnmt-ozan
-cd ~/experiments/simnmt/nmtpy/w2w_models/en-de
+cd ~/experiments/simnmt/nmtpy/w2w_models/en-fr
 ls
-/vol/bitbucket/al4616/simmt/scripts/decode_nmt_test_waitk.sh
+/vol/bitbucket/al4616/simmt/scripts/decode_snmt_waitk.sh
 /vol/bitbucket/al4616/simmt/scripts/delay_analysis.py -r /vol/bitbucket/al4616/simmt/data/multi30k/en-fr/test_2016_flickr.lc.norm.tok.fr
