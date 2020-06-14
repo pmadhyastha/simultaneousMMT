fb39761926734c460eb60fa1f2118f923a9ce47c
Andy Li <andy.li@artificial.io>
2020-06-14 19:52:02 +0100

    added shuffled en-fr confs, and roi_feat agent side multimodal conf

---
+++ slurm_scripts/multimodal/en-fr-multimodal-shuffled-waitk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=al4616
+#SBATCH -o ./simultaneousMMT/experiments/custom/en-fr-multimodal-shuffled-waitk-slurm-%j.out
+
+export PATH=/vol/bitbucket/al4616/miniconda3/bin/:$PATH
+source activate
+conda activate simnmt-ozan
+cd simmt
+TERM=xterm
+for k in 1 2 3 5 7
+do
+    CUDA_VISIBLE_DEVICES=0 nmtpy train -C ../simultaneousMMT/custom_configs/en-fr/wait$k-rnn-multimodal-shuffled-decatt.conf
+    /usr/bin/nvidia-smi
+done
+uptime
